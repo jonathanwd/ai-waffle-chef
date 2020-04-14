9aa342388c89ba6677a623b1fa94f192c13b8e74
Suguru Onda <47617338+suguruonda@users.noreply.github.com>
2020-04-14 16:25:26 -0600

    Update make.py

--- make.py
+++ make.py
@@ -6,6 +6,7 @@ from parts.parings import pair
 from parts.evaluators import waffleness_estimator
 from parts.surprise import surprise_score
 from parts.food_pair_probability import pair_score
+from parts.gpt2Chef import ingredientIdeas_gpt2, find_ingredients
 from random import randint
 import gensim
 import json
@@ -17,37 +18,11 @@ import copy
 
 DEBUG = False
 
-def cook(inspiration, model):
+def generate_recipe_objects(possibilities):
     new_ingredients = []
-    print("You want me to make a " + inspiration + " waffle?")
-    # additional_keywd = random.choice(["butter","meal","bake", "ingredients", "flour"])
-    flour_ideas = ingredientIdeas(model, [inspiration, "flour"], [])
-    butter_ideas = ingredientIdeas(model, [inspiration, "butter"], [])
-    sugar_ideas = ingredientIdeas(model, [inspiration, "sugar"], [])
-    ingredient_ideas = ingredientIdeas(model, [inspiration, "ingredient"], [])
-    meal_ideas = ingredientIdeas(model, [inspiration, "meal"], [])
-    bake_ideas = ingredientIdeas(model, [inspiration, "bake"], [])
-    spice_ideas = ingredientIdeas(model, [inspiration, "spice"], [])
-    topping_ideas = ingredientIdeas(model, [inspiration, "topping"], [])
-    ideas = flour_ideas + butter_ideas + sugar_ideas + ingredient_ideas + meal_ideas + bake_ideas + spice_ideas + topping_ideas
-    ideas = set(ideas)
-    # ideas = ingredientIdeas(model, [inspiration, additional_keywd], [])
-    possibilities = []
-    for idea in ideas:
-        idea = idea[0].replace('_', ' ').lower()
-        possibilities.append(idea)
-        pieces = idea.split(" ", 1)
-        if len(pieces) == 2:
-            if pieces[0] not in possibilities:
-                possibilities.append(pieces[0])
-            if pieces[1] not in possibilities:
-                possibilities.append(pieces[1])
-        if DEBUG:print(idea)
-    possibilities = set(possibilities)
-    if DEBUG:print(possibilities)
     for possibility in possibilities:
         classed = classify(possibility)
-        if not classed: 
+        if not classed:
             classed = classify(possibility + "s")
         if not classed:
             classed = classify(possibility[:-1])
@@ -55,7 +30,7 @@ def cook(inspiration, model):
             a = int(get_amount(possibility))
             if a > 0:
                 if DEBUG:print(classed)
-                if DEBUG:print(possibility)
+                print(possibility)
                 new_ingredient = ingredientClass()
                 new_ingredient.define_me(possibility, a, classed)
                 new_ingredients.append(new_ingredient)
@@ -63,6 +38,51 @@ def cook(inspiration, model):
             pass
             #print("no")
     return new_ingredients
+
+def cook(inspiration, model, model_sel):
+    possibilities = []
+    print("You want me to make a " + inspiration + " waffle?")
+    key_ingredients = []
+    ESSENCIAL_INGREDIENT = False
+    key_ingredients = find_ingredients(inspiration)
+    if len(key_ingredients) >0:
+        ESSENCIAL_INGREDIENT = True
+
+    if model_sel == 0:
+        # additional_keywd = random.choice(["butter","meal","bake", "ingredients", "flour"])
+        flour_ideas = ingredientIdeas(model, [inspiration, "flour"], [])
+        butter_ideas = ingredientIdeas(model, [inspiration, "butter"], [])
+        sugar_ideas = ingredientIdeas(model, [inspiration, "sugar"], [])
+        ingredient_ideas = ingredientIdeas(model, [inspiration, "ingredient"], [])
+        meal_ideas = ingredientIdeas(model, [inspiration, "meal"], [])
+        bake_ideas = ingredientIdeas(model, [inspiration, "bake"], [])
+        spice_ideas = ingredientIdeas(model, [inspiration, "spice"], [])
+        topping_ideas = ingredientIdeas(model, [inspiration, "topping"], [])
+        ideas = flour_ideas + butter_ideas + sugar_ideas + ingredient_ideas + meal_ideas + bake_ideas + spice_ideas + topping_ideas
+        ideas = set(ideas)
+        # ideas = ingredientIdeas(model, [inspiration, additional_keywd], [])
+        for idea in ideas:
+            idea = idea[0].replace('_', ' ').lower()
+            possibilities.append(idea)
+            pieces = idea.split(" ", 1)
+            if len(pieces) == 2:
+                if pieces[0] not in possibilities:
+                    possibilities.append(pieces[0])
+                if pieces[1] not in possibilities:
+                    possibilities.append(pieces[1])
+            if DEBUG:print(idea)
+    elif model_sel == 1:
+        possibilities = ingredientIdeas_gpt2(inspiration)
+    possibilities = set(possibilities)
+    key_ingredients_object = []
+    if ESSENCIAL_INGREDIENT:
+        for i in key_ingredients:
+            if i in possibilities:
+                possibilities.remove(i)
+        key_ingredients_object = generate_recipe_objects(key_ingredients)
+    if DEBUG:print(possibilities)
+    new_ingredients_object = generate_recipe_objects(possibilities)
+    return new_ingredients_object, key_ingredients_object
 
 def select(new_ingredients):
     number = 5
@@ -75,7 +95,7 @@ def select(new_ingredients):
     #     random.shuffle(ideas)
     #     for idea in ideas:
     #         classed = classify(idea)
-    #         if not classed: 
+    #         if not classed:
     #             classed = classify(idea + "s")
     #         if not classed:
     #             classed = classify(idea[:-1])
@@ -88,17 +108,32 @@ def select(new_ingredients):
     #                 break
     # return(randos + suggestions)
 
-def select_all_possible_pair(new_ingredients):
+def select_all_possible_pair(new_ingredients,key_ingredients):
     result = []
-    upper_limit = min(6, len(new_ingredients))
-    for n in range(5,upper_limit):
+    key_num = len(key_ingredients)
+    if key_num > 0:
+        if key_num > 4:
+            lower_limit = 2
+        else:
+            lower_limit = 5 - key_num
+    else:
+        lower_limit = 5
+    upper_limit = lower_limit + 1
+    #upper_limit = min(upper_limit, len(new_ingredients))
+    for n in range(lower_limit,upper_limit):
         for conb in itertools.combinations(new_ingredients, n):
             temp_list = []
             for c in conb:
                 temp = copy.copy(c)
                 temp_list.append(temp)
+            if key_num > 0:
+                sel_key_ing = random.sample(key_ingredients, min(3, key_num))
+                for s in sel_key_ing:
+                    temp = copy.copy(s)
+                    temp_list.append(temp)
             result.append(temp_list)
             del temp_list
+    print("Number of key ingredient:                        " + str(len(key_ingredients)))
     print("Number of inspired additional ingredient:        " + str(len(new_ingredients)))
     print("Number of possible combination of recipes:       " + str(len(result)))
     return result
@@ -119,22 +154,23 @@ def foodpair_score_to_percentile(score):
 
 # surprise_percentiles = [0.412, 0.490, 0.512, 0.528, 0.545, 0.565, 0.580, 0.601, 0.624, 0.654, 0.748]
 # foodpair_percentiles = [11.0, 15.4, 16.8, 17.6, 18.3, 19.0, 19.8, 20.7, 21.7, 23.1, 27.1]
-def one_recipe(model, surprise, foodpair, people, inspiration):
-    if not wordcheck(model,inspiration):
-        raise Exception("Error: " + inspiration + ' was not found in the model. Please try again.')
+def one_recipe(model, surprise, foodpair, people, inspiration, model_sel, meat_option, nuts_option, dairy_option):
+    if model_sel == 0:
+        if not wordcheck(model,inspiration):
+            raise Exception("Error: " + inspiration + ' was not found in the model. Please try again.')
     surprise_rating = int((surprise-1)/10)
     foodpair_rating = int((foodpair-1)/10)
     multiplier = people / 2
-    new_ingredients = cook(inspiration, model)
+    new_ingredients, key_ingredients = cook(inspiration, model, model_sel)
     #selected_ingredients = select(new_ingredients)
-    list_of_selected_ingredients = select_all_possible_pair(new_ingredients)
+    list_of_selected_ingredients = select_all_possible_pair(new_ingredients, key_ingredients)
     count = len(list_of_selected_ingredients)
     random_location = randint(0, count-1)
     location = random_location
     # loop through all recipes starting at random location
-    while True:  
+    while True:
         selected_ingredients = list_of_selected_ingredients[location]
-        initial_ingredients = generate_recipe()
+        initial_ingredients = generate_recipe(dairy_option)
         ingredient_strings = []
         for ingredient in selected_ingredients:
             ingredient_strings.extend(ingredient.get_name().split())
@@ -142,6 +178,7 @@ def one_recipe(model, surprise, foodpair, people, inspiration):
         p_score = pair_score(initial_ingredients + selected_ingredients)
 
         recipe = recipeClass()
+        recipe.add_category(meat_option, nuts_option, dairy_option)
         for i in initial_ingredients:
             i.multiply(multiplier)
             recipe.add_ingredient(i)
@@ -164,13 +201,13 @@ def one_recipe(model, surprise, foodpair, people, inspiration):
             print('')
             print(recipe.print_recipe())
             return recipe
-        location = location + 1  
+        location = location + 1
         location = location % count
-        if(location == random_location):  
+        if(location == random_location):
             print("No recipe found for specified inputs.")
-            break  
+            break
 
-def create(model, surprise, foodpair, people):
+def create(model, surprise, foodpair, people, model_sel, meat_option, nuts_option, dairy_option):
     #logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     surprise_percentiles = [0.412, 0.490, 0.512, 0.528, 0.545, 0.565, 0.580, 0.601, 0.624, 0.654, 0.748]
     foodpair_percentiles = [11.0, 15.4, 16.8, 17.6, 18.3, 19.0, 19.8, 20.7, 21.7, 23.1, 27.1]
@@ -184,13 +221,16 @@ def create(model, surprise, foodpair, people):
             if inspiration.lower() == "no":
                 print("OK, see you!")
                 sys.exit()
-            if wordcheck(model,inspiration):
+            if model_sel == 0:
+                if wordcheck(model,inspiration):
+                    break
+            else:
                 break
-        new_ingredients = cook(inspiration, model)
+        new_ingredients, key_ingredients = cook(inspiration, model,model_sel)
         #selected_ingredients = select(new_ingredients)
-        list_of_selected_ingredients = select_all_possible_pair(new_ingredients)        
+        list_of_selected_ingredients = select_all_possible_pair(new_ingredients, key_ingredients)
         for selected_ingredients in list_of_selected_ingredients:
-            initial_ingredients = generate_recipe()
+            initial_ingredients = generate_recipe(dairy_option)
             ingredient_strings = []
             for ingredient in selected_ingredients:
                 ingredient_strings.extend(ingredient.get_name().split())
@@ -198,6 +238,8 @@ def create(model, surprise, foodpair, people):
             p_score = pair_score(initial_ingredients + selected_ingredients)
 
             recipe = recipeClass()
+            recipe.add_category(meat_option, nuts_option, dairy_option)
+
             for i in initial_ingredients:
                 i.multiply(multiplier)
                 recipe.add_ingredient(i)
@@ -222,10 +264,15 @@ def create(model, surprise, foodpair, people):
                 del initial_ingredients
                 del recipe
 
-# # Uncomment this section to run without the GUI
-# # model = gensim.models.KeyedVectors.load_word2vec_format('data/GoogleNews-vectors-negative300.bin', binary=True)
-# model = gensim.models.KeyedVectors.load_word2vec_format('data/GoogleNews-vectors-negative300.bin', limit=1600000, binary=True)
-# surprise = 90 
-# foodpair = 50
-# people = 3
-# create(model, surprise, foodpair, people)
+if __name__ == '__main__':
+    # # Uncomment this section to run without the GUI
+    # # model = gensim.models.KeyedVectors.load_word2vec_format('data/GoogleNews-vectors-negative300.bin', binary=True)
+    model = gensim.models.KeyedVectors.load_word2vec_format('data/GoogleNews-vectors-negative300.bin', limit=1600000, binary=True)
+    surprise = 50
+    foodpair = 30
+    people = 3
+    model_sel = 0
+    meat_option = False
+    nuts_option = False
+    dairy_option = False
+    create(model, surprise, foodpair, people, model_sel, meat_option, nuts_option, dairy_option)
